bf81c63caa4c8ce6d0abd0855b33ad3138e608a0
BaylorRice <87844576+BaylorRice@users.noreply.github.com>
2023-04-20 16:30:27 -0500

    Created Tile.h

---
+++ !Source Code/Tile.h
@@ -0,0 +1,33 @@
+#ifndef TILE_H_
+#define TILE_H_
+
+#include "SDL_Plotter.h"
+#include "Constants.h"
+
+class Tile {
+    private:
+    int size;
+    color tile_color;
+    point loc, prevLoc;
+
+    public:
+    Tile();
+    Tile(int);
+    Tile(int, color);
+
+    void setSize(int);
+    void setLoc(point);
+    void setColor(color);
+
+    int getSize() const;
+    point getLoc() const;
+    color getColor() const;
+
+    void draw(SDL_Plotter&) const;
+
+    void moveDown();
+    void moveRight();
+    void moveLeft();
+};
+
+#endif TILE_H_
