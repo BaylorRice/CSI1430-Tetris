a8b1a7d169e57ed5897433f0173bd44465814827
BaylorRice <87844576+BaylorRice@users.noreply.github.com>
2023-04-20 15:42:04 -0500

    Reorganize Files

---
+++ Source Code/main.cpp
@@ -0,0 +1,30 @@
+/*
+* Authors:
+* Assignment Title:
+* Assignment Description:
+* Due Date:
+* Date Created:
+* Date Last Modified:
+*/
+
+#include <iostream>
+#include <cmath>
+#include "SDL_Plotter.h"
+
+using namespace std;
+
+const int XSIZE = 500;
+const int YSIZE = 500;
+
+int main(int argc, char** argv) {
+
+    SDL_Plotter g(XSIZE, YSIZE);
+    while (!g.getQuit()) {
+
+        // Code Here...
+
+        g.update();
+    }
+
+    return 0;
+}
