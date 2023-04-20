4fcb4ab19320a85e67e4401f3250ec1aac642079
BaylorRice <87844576+BaylorRice@users.noreply.github.com>
2023-04-20 14:56:41 -0500

    More testing

---
+++ Source Code/Tetris/tetris.example/tetris.example/tetris.example/constant.h
@@ -0,0 +1,22 @@
+//
+//  constant.h
+//  tetris.example
+//
+//  Created by josietwirls on 3/30/23.
+//
+
+#ifndef constant_h
+#define constant_h
+
+#include "SDL_Plotter.h"
+
+
+const int NUM_ROW = 800;
+const int NUM_COL = 500;
+
+const color YELLOW     = color(220,220,20);
+const color RED        = color(220,20,20);
+const color BLK        = color(0,0,0);
+const color BACKGROUND = color(250,250,250);
+
+#endif /* constant_h */
