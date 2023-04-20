f99de7dc83ab01d5a2bc203469146089cf4b8df9
BaylorRice <87844576+BaylorRice@users.noreply.github.com>
2023-04-20 16:33:46 -0500

    Tile set functions

--- !Source Code/Tile.cpp
+++ !Source Code/Tile.cpp
@@ -5,3 +5,15 @@ Tile::Tile(int inSize = 50, color inColor = YELLOW, point inLoc = point(0,0)) {
     tile_color = inColor;
     loc = inLoc;
 }
+
+void Tile::setSize(int inSize) {
+    size = inSize;
+}
+
+void Tile::setLoc(point inLoc) {
+    loc = inLoc;
+}
+
+void Tile::setColor(color inColor) {
+    tile_color = inColor;
+}
