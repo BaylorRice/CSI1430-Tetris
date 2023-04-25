14016d4183c07a14cdfa7228aded8f6f727dab4a
BaylorRice <87844576+BaylorRice@users.noreply.github.com>
2023-04-24 19:28:31 -0500

    LeftL Block Class

--- !Source Code/LeftL.h
+++ !Source Code/LeftL.h
@@ -13,5 +13,29 @@ using namespace std;
 #ifndef LEFTL_H_INCLUDED
 #define LEFTL_H_INCLUDED
 
+class Block_LeftL {
+    private:
+    point loc;
+    vector<point> prevLoc;
+    Tile a, b, c, d;
+    color block_color;
+    bool isDelete = false;
+
+    public:
+    Block_LeftL();
+    Block_LeftL(point inLoc, color inColor);
+
+    void rotateClock();
+    void rotateCounterClock();
+
+    void moveDown(vector<Tile>& others);
+
+    void strafeToMouse(point mouseLoc);
+
+    void atBottom();
+    void sitting(vector<Tile>& others);
+
+    void remove(vector<Tile>& others);
+};
 
 #endif // LEFTL_H_INCLUDED
