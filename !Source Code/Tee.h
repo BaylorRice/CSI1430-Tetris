0d6b6dc8056afd7c1ee20ed7379a1b5cbf000e59
BaylorRice <87844576+BaylorRice@users.noreply.github.com>
2023-05-01 12:14:23 -0500

    Started Tee Block
    I'm saying "Tee" because just using the letter T would probably be bad

---
+++ !Source Code/Tee.h
@@ -0,0 +1,61 @@
+/*
+* Authors: Reese Ford, ...
+* Assignment Title: Group Project - Tetris
+* Assignment Description: INSERT DESCRIPTION
+* Due Date: INSERT DUE DATE
+* Date Created: 05/01/2023
+* Date Last Modified: 05/01/2023
+*/
+
+#include <iostream>
+#include "Tile.h"
+#include "SDL_Plotter.h"
+using namespace std;
+
+#ifndef TEE_H_INCLUDED
+#define TEE_H_INCLUDED
+
+class Block_Tee {
+    private:
+    point loc;
+    vector<point> prevLoc;
+    Tile a, b, c, d;
+    color block_color;
+    int rotation = 1;
+    bool isDelete = false;
+
+    public:
+    // Specific Constructors
+    Block_Tee();
+    Block_Tee(point inLoc, int inRot, color inColor);
+
+    // Block-Universal Functions
+    void setColor(color inColor);
+
+    point getLoc() const;
+    color getColor() const;
+
+    void draw(SDL_Plotter& g);
+
+    void rotateClock(vector<Tile>& others);
+    void rotateCounterClock(vector<Tile>& others);
+
+    void moveDown(vector<Tile>& others);
+    void snapToBottom(vector<Tile>& others);
+
+    int snapLateral(int in, int min, int max, int interval);
+
+    // LeftL-Specific Functions
+    void setLoc(point inLoc);
+
+    void strafeToMouse(point mouseLoc, vector<Tile>& others);
+
+    bool atBottom();
+    bool sitting(vector<Tile>& others);
+    void touchingTileSides(vector<Tile>& others, bool& touchingLeft, bool& touchingRight);
+
+    void remove(vector<Tile>& others, SDL_Plotter& g); // Removes the Tiles from this object's "supervision"
+
+};
+
+#endif // TEE_H_INCLUDED
