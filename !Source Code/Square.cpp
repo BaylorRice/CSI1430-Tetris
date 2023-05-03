64b414310573518ff7c75c2e3f5a5b8c0be8b3c8
mjruhman <128708542+mjruhman@users.noreply.github.com>
2023-05-03 14:28:48 -0500

    Update Square.cpp

--- !Source Code/Square.cpp
+++ !Source Code/Square.cpp
@@ -30,30 +30,11 @@ void Block_Square::setLoc(point inLoc) {
     loc = inLoc;
 
     if (rotation == 1) {
-        a.setLoc(loc);
-        b.setLoc(point(loc.x - SIZE, loc.y));
-        c.setLoc(point(loc.x, loc.y - SIZE));
-        d.setLoc(point(loc.x, loc.y - (2 * SIZE)));
-    }
-    else if (rotation == 2) {
-        a.setLoc(point(loc.x - SIZE, loc.y));
-        b.setLoc(point(loc.x - SIZE, loc.y - SIZE));
-        c.setLoc(loc);
-        d.setLoc(point(loc.x + SIZE, loc.y));
-    }
-    else if (rotation == 3) {
         a.setLoc(point(loc.x - SIZE, loc.y - SIZE));
         b.setLoc(point(loc.x, loc.y - SIZE));
         c.setLoc(point(loc.x - SIZE, loc.y));
-        d.setLoc(point(loc.x - SIZE, loc.y + SIZE));
+        d.setLoc(loc);
     }
-    else if (rotation == 4) {
-        a.setLoc(point(loc.x, loc.y - SIZE));
-        b.setLoc(loc);
-        c.setLoc(point(loc.x - SIZE, loc.y - SIZE));
-        d.setLoc(point(loc.x - (2 * SIZE), loc.y - SIZE));
-    }
-
 }
 
 void Block_Square::setColor(color inColor) {
@@ -129,52 +110,36 @@ void Block_Square::strafeToMouse(point mouseLoc, vector<Tile>& others) {
     touchingTileSides(others, touchingLeft, touchingRight);
     if (!touchingLeft && !touchingRight) {
         // MOVE FREELY
-        if ((rotation == 1) || (rotation == 3)) {
+        if (rotation == 1) {
             p.x = snapLateral(mouseLoc.x, SIZE, NUM_COL - SIZE, SIZE);
-        }
-        else if (rotation == 2) {
-            p.x = snapLateral(mouseLoc.x, SIZE, NUM_COL - (2 * SIZE), SIZE);
-        }
-        else if (rotation == 4) {
-            p.x = snapLateral(mouseLoc.x, (2 * SIZE), NUM_COL - SIZE, SIZE);
         }
     }
     else if (touchingLeft && !touchingRight) {
         // MOVE RIGHT ONLY
-        if ((rotation == 1) || (rotation == 3)) {
-            p.x = snapLateral(mouseLoc.x, loc.x, NUM_COL - SIZE, SIZE);
-        }
-        else if (rotation == 2) {
-            p.x = snapLateral(mouseLoc.x, loc.x, NUM_COL - (2 * SIZE), SIZE);
-        }
-        else if (rotation == 4) {
+        if (rotation == 1) {
             p.x = snapLateral(mouseLoc.x, loc.x, NUM_COL - SIZE, SIZE);
         }
     }
     else {
         // MOVE LEFT ONLY
-        if ((rotation == 1) || (rotation == 3)) {
+        if (rotation == 1) {
             p.x = snapLateral(mouseLoc.x, SIZE, loc.x, SIZE);
         }
-        else if (rotation == 2) {
-            p.x = snapLateral(mouseLoc.x, SIZE, loc.x, SIZE);
-        }
-        else if (rotation == 4) {
-            p.x = snapLateral(mouseLoc.x, (2 * SIZE), loc.x, SIZE);
-        }
+    }
+    // Speed Limiter (Forces the block to only move SIZE pixels a cycle)
+    if (p.x < loc.x) {
+        p.x = loc.x - SIZE;
+    }
+    else if (p.x > loc.x) {
+        p.x = loc.x + SIZE;
     }
     setLoc(p);
 }
 
 bool Block_Square::atBottom() {
     bool atBottom = false;
-    if (rotation != 3) {
+    if (rotation == 1) {
         if (getLoc().y >= NUM_ROW - SIZE) {
-            atBottom = true;
-        }
-    }
-    else if (rotation == 3) {
-        if (getLoc().y >= NUM_ROW - (2 * SIZE)) {
             atBottom = true;
         }
     }
@@ -194,51 +159,6 @@ bool Block_Square::sitting(vector<Tile>& others) {
             }
         }
     }
-    else if (rotation == 2) {
-        for (int i = 0; i < others.size(); i++) {
-            if ((loc.x == others.at(i).getLoc().x) ||
-                ((loc.x - SIZE) == others.at(i).getLoc().x) ||
-                ((loc.x + SIZE) == others.at(i).getLoc().x)) {
-                if (loc.y + SIZE == others.at(i).getLoc().y) {
-                    sit = true;
-                }
-            }
-        }
-    }
-    else if (rotation == 3) {
-        for (int i = 0; i < others.size(); i++) {
-            if (loc.x - SIZE == others.at(i).getLoc().x) {
-                if (loc.y + (2 * SIZE) == others.at(i).getLoc().y) {
-                    sit = true;
-                }
-            }
-            if (loc.x == others.at(i).getLoc().x) {
-                if (loc.y == others.at(i).getLoc().y) {
-                    sit = true;
-                }
-            }
-        }
-    }
-    else if (rotation == 4) {
-        for (int i = 0; i < others.size(); i++) {
-            if (loc.x == others.at(i).getLoc().x) {
-                if (loc.y + SIZE == others.at(i).getLoc().y) {
-                    sit = true;
-                }
-            }
-            if (loc.x - SIZE == others.at(i).getLoc().x) {
-                if (loc.y == others.at(i).getLoc().y) {
-                    sit = true;
-                }
-            }
-            if (loc.x - (2 * SIZE) == others.at(i).getLoc().x) {
-                if (loc.y == others.at(i).getLoc().y) {
-                    sit = true;
-                }
-            }
-        }
-    }
-
     return sit;
 }
 
@@ -247,99 +167,19 @@ void Block_Square::touchingTileSides(vector<Tile>& others, bool& touchingLeft, b
     touchingRight = false;
     if (rotation == 1) {
         for (int i = 0; i < others.size(); i++) {
-            if (loc.x - SIZE == others.at(i).getLoc().x) {
-                if (loc.y - (2 * SIZE) == others.at(i).getLoc().y) { // LEFT TOP
-                    touchingLeft = true;
-                }
-                else if (loc.y - SIZE == others.at(i).getLoc().y) { // LEFT MIDDLE
-                    touchingLeft = true;
-                }
-            }
-            else if (loc.x - (2 * SIZE) == others.at(i).getLoc().x) {
-                if (loc.y == others.at(i).getLoc().y) { // LEFT BOTTOM
-                    touchingLeft = true;
-                }
-            }
-            else if (loc.x + SIZE == others.at(i).getLoc().x) {
-                if (loc.y - (2 * SIZE) == others.at(i).getLoc().y) { // RIGHT TOP
-                    touchingRight = true;
-                }
-                else if (loc.y - SIZE == others.at(i).getLoc().y) { // RIGHT MIDDLE
-                    touchingRight = true;
-                }
-                else if (loc.y == others.at(i).getLoc().y) { // RIGHT BOTTOM
-                    touchingRight = true;
-                }
-            }
-        }
-    }
-    else if (rotation == 2) {
-        for (int i = 0; i < others.size(); i++) {
             if (loc.x - (2 * SIZE) == others.at(i).getLoc().x) {
+                if (loc.y  == others.at(i).getLoc().y) { // LEFT BOTTOM
+                    touchingLeft = true;
+                }
                 if (loc.y - SIZE == others.at(i).getLoc().y) { // LEFT TOP
                     touchingLeft = true;
                 }
-                else if (loc.y == others.at(i).getLoc().y) { // LEFT BOTTOM
-                    touchingLeft = true;
-                }
             }
-            else if (loc.x == others.at(i).getLoc().x) {
+            if (loc.x + SIZE == others.at(i).getLoc().x) {
                 if (loc.y - SIZE == others.at(i).getLoc().y) { // RIGHT TOP
                     touchingRight = true;
                 }
-            }
-            else if (loc.x + (2 * SIZE) == others.at(i).getLoc().x) {
                 if (loc.y == others.at(i).getLoc().y) { // RIGHT BOTTOM
-                    touchingRight = true;
-                }
-            }
-        }
-    }
-    else if (rotation == 3) {
-        for (int i = 0; i < others.size(); i++) {
-            if (loc.x - (2 * SIZE) == others.at(i).getLoc().x) {
-                if (loc.y - SIZE == others.at(i).getLoc().y) { // LEFT TOP
-                    touchingLeft = true;
-                }
-                else if (loc.y == others.at(i).getLoc().y) { // LEFT MIDDLE
-                    touchingLeft = true;
-                }
-                else if (loc.y + SIZE == others.at(i).getLoc().y) { // LEFT BOTTOM
-                    touchingLeft = true;
-                }
-            }
-            else if (loc.x + SIZE == others.at(i).getLoc().x) {
-                if (loc.y - SIZE == others.at(i).getLoc().y) { // RIGHT TOP
-                    touchingRight = true;
-                }
-            }
-            else if (loc.x == others.at(i).getLoc().x) {
-                if (loc.y == others.at(i).getLoc().y) { // RIGHT MIDDLE
-                    touchingRight = true;
-                }
-                else if (loc.y + SIZE == others.at(i).getLoc().y) { // RIGHT BOTTOM
-                    touchingRight = true;
-                }
-            }
-        }
-    }
-    else if (rotation == 4) {
-        for (int i = 0; i < others.size(); i++) {
-            if (loc.x - (3 * SIZE) == others.at(i).getLoc().x) {
-                if (loc.y - SIZE == others.at(i).getLoc().y) { // LEFT TOP
-                    touchingLeft = true;
-                }
-            }
-            else if (loc.x - SIZE == others.at(i).getLoc().x) {
-                if (loc.y == others.at(i).getLoc().y) { // LEFT BOTTOM
-                    touchingLeft = true;
-                }
-            }
-            else if (loc.x + SIZE == others.at(i).getLoc().x) {
-                if (loc.y - SIZE == others.at(i).getLoc().y) { // RIGHT TOP
-                    touchingRight = true;
-                }
-                else if (loc.y == others.at(i).getLoc().y) { // RIGHT BOTTOM
                     touchingRight = true;
                 }
             }
